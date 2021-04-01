afc1002fefd506b5115a0e07adefefc19d9389c3
Waqar <waqar@waqar.co>
2021-04-01 15:31:44 +0500

    cleaned code ready to start using google API in the next task

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,7 +2,12 @@
 
 require 'csv'
 
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
 File.exist?('event_attendees.csv')
+puts 'EventManager Initialised'
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -11,6 +16,8 @@ contents = CSV.open(
 )
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
   puts "#{name} #{zipcode}"
 end
