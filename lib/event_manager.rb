fa4d0fc5efcd3ff13540e4aaa046ce64bd660ad3
Waqar Mohammad <waqar@waqar.co>
2021-04-02 14:50:57 +0500

    completed the task to highlight what hour most bookings were made

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,6 +3,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'date'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -17,6 +18,17 @@ def clean_phone_number(phone_number)
   else
     phone_number
   end
+end
+
+def clean_date(time)
+  DateTime.strptime(time, '%m/%d/%y %H:%M').hour
+end
+
+def most_active_hour(arr)
+  hour_counts = arr.each_with_object(Hash.new(0)) do |hour, new_hash|
+   new_hash[hour] += 1
+  end
+  return hour_counts.each { |k, v| puts "Most active hour is: #{k}, where #{v} people registered" if v == hour_counts.values.max }
 end
 
 def legislators_by_zipcode(zipcode)
@@ -55,20 +67,26 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+hours = []
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   phone_number = row[:homephone]
 
+  reg_date_hour_finder = clean_date(row[:regdate])
+
+  hours << reg_date_hour_finder
+  
   phone_number = clean_phone_number(phone_number)
 
   zipcode = clean_zipcode(row[:zipcode])
 
   # legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{id} #{name} #{phone_number} #{zipcode}"
-
   # form_letter = erb_template.result(binding)
 
   # save_thank_you_letter(id, form_letter)
 end
+
+most_active_hour(hours)
--- test.rb
+++ test.rb
@@ -1,22 +1,4 @@
-# frozen_string_literal: false
-
-def clean_phone_number(phone_number)
-  phone_number = phone_number.to_s
-  if phone_number.length < 10 || phone_number.length > 11 || phone_number.length == 11 && phone_number[0] != '1'
-    'Bad Number'
-  elsif phone_number[0] == '1' && phone_number.length == 11
-    phone_number[1..10]
-  else
-    phone_number
-  end
+def clean_date(time)
+  hour = DateTime.strptime(time, '%m/%d/%y %H:%M')
+  hour = hour.hour
 end
-
-phone_number = 12345678900
-puts clean_phone_number(phone_number)
-
-
-# If the phone number is less than 10 digits, assume that it is a bad number
-# If the phone number is 10 digits, assume that it is good
-  # If the phone number is 11 digits and the first number is 1, trim the 1 and use the first 10 digits
-  # If the phone number is 11 digits and the first number is not 1, then it is a bad number
-# If the phone number is more than 11 digits, assume that it is a bad number
